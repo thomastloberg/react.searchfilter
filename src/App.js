05f7f02ba1310189fa4fcb29252a2f96204e9059
Thomas T. Løberg <thtech93@gmail.com>
2022-05-19 19:50:41 +0200

    Works!

--- src/App.js
+++ src/App.js
@@ -11,12 +11,31 @@ function App() {
     "Watermelon"
   ];
 
+  const [filterList, setFilterList] = useState(list);
+
+  const handleSearch = (event) => {
+    if(event.target.value === "") {
+      setFilterList(list);
+      return
+    }
+
+    const filteredvalues = list.filter(
+      (item) =>
+        item.toLocaleLowerCase().indexOf(event.target.value.toLocaleLowerCase()) !== -1
+    );
+    
+    setFilterList(filteredvalues);
+  }
+
   return (
     <div className='app'>
       <div>
-        Search: <input name='query' type='text'/>
+        Search: <input name='query' type='text' onChange={handleSearch}/>
       </div>
       
+      {filterList && filterList.map((item, index) => (
+        <div key={index}>{item}</div>
+      ))}
     </div>
   );
 }
